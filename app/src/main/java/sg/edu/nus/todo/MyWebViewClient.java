ea3a9338103c06d23db175cbbf430e2dfdfd0997
gsm99 <gohserming@gmail.com>
2015-10-27 12:20:04 +0800

    intermediate 2

---
+++ app/src/main/java/sg/edu/nus/todo/MyWebViewClient.java
@@ -0,0 +1,13 @@
+private class MyWebViewClient extends WebViewClient {
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        if (Uri.parse(url).getHost().equals("www.example.com")) {
+            // This is my web site, so do not override; let my WebView load the page
+            return false;
+        }
+        // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
+        return true;
+    }
+}
