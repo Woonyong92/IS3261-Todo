6135ba841bc3ea21290d1da570494af5b92f2944
gsm99 <gohserming@gmail.com>
2015-10-27 14:35:38 +0800

    added files/folders for hybrid and linked up the pages

---
+++ app/src/main/java/sg/edu/nus/todo/Tutorial.java
@@ -0,0 +1,52 @@
+package sg.edu.nus.todo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+public class Tutorial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial);
+
+        WebView wv = (WebView)findViewById(R.id.webView1);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.setWebChromeClient(new WebChromeClient());
+        wv.loadUrl("file:///android_asset/www/myHTML.html");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_tutorial, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void goBack(View view){
+        Intent goBack = new Intent(this, MainActivity.class);
+        startActivity(goBack);
+    }
+}
